e1181dce4be5a3e44f20b26f3dd408596bd82160
Juyeon Kim <jay.jykim91@gmail.com>
2020-12-26 18:20:09 +0900

    counter finished

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import { createStore } from 'redux';
+
+const plus = document.querySelector('#plus');
+const minus = document.querySelector('#minus');
+const number = document.querySelector('span');
+
+number.innerText = 0;
+
+const PLUS = "plus";
+const MINUS = "minus";
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case PLUS:
+      return count + 1
+    case MINUS:
+      return count -1
+    default:
+      return count
+  }
+}
+
+const countStore = createStore(countModifier);
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+countStore.subscribe(onChange);
+
+plus.addEventListener('click', () => countStore.dispatch({ type: PLUS }));
+minus.addEventListener('click', () => countStore.dispatch({ type: MINUS }));
